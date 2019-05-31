a078880b35fba61964b1758ee89b5f2f6670f3d3
joelcrawford <joelrootcrawford@gmail.com>
2019-05-31 10:27:24 -0700

    i8n updates

---
+++ build/precache-manifest.a489d199cc2610a1a711ddc6a866de9f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "860e9d00bbd99606ccae",
+    "url": "/static/css/main.dbd3e0f7.chunk.css"
+  },
+  {
+    "revision": "860e9d00bbd99606ccae",
+    "url": "/static/js/main.860e9d00.chunk.js"
+  },
+  {
+    "revision": "a091c47b8e872e99f707",
+    "url": "/static/js/1.a091c47b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "67ac4b0f05e70db2662fe1400136a717",
+    "url": "/static/media/UB-woodblock-closeup.67ac4b0f.jpg"
+  },
+  {
+    "revision": "9631b4466075c75e8d35222cfdec38da",
+    "url": "/static/media/checker-pattern.9631b446.svg"
+  },
+  {
+    "revision": "6a2bf66b6d22d46764f66e21a652a4a3",
+    "url": "/index.html"
+  }
+];

6035d2162eb37c8443441184ac3f985c8370ac6b
joelcrawford <joelrootcrawford@gmail.com>
2019-11-27 08:50:49 -0800

    first pass at moving to express/nginx

--- src/index.js
+++ src/index.js
@@ -1,66 +1,74 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { Route, BrowserRouter as Router } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
-import { 
-    fetchPages, fetchPosts, 
-    fetchData, fetchAuthors, fetchTopics,
-    getGS } from './store/actions'
+import {
+  fetchPages,
+  fetchPosts,
+  fetchData,
+  fetchAuthors,
+  fetchTopics,
+  fetchCollections,
+  getGS
+} from "./store/actions";
 
-import RouteSwitch from './components/router/RouteSwitch'
+import RouteSwitch from "./components/router/RouteSwitch";
 
-import configureStore from './store/configureStore'
-import { checkConnection } from './store/connection'
+import configureStore from "./store/configureStore";
+import { checkConnection } from "./store/connection";
 
-import './assets/css/main.css'
-import './assets/css/index.css'
+import "./assets/css/main.css";
+import "./assets/css/index.css";
 
 // this is what gives the entire app access to t, i18n...
-import './store/localization/i18n'
+import "./store/localization/i18n";
 
-const store = configureStore()
+const store = configureStore();
 
-// log('initial state index.js', store.getState()) 
+// log('initial state index.js', store.getState())
 // THIS COULD BE REPLACED WITH ONE 'INITIALIZE APP' type function
 // GET PAGES, POSTS, ES DATA
 
+store.dispatch(fetchCollections());
+
 // Wordpress data
-store.dispatch(fetchPages())
-store.dispatch(fetchPosts())
+store.dispatch(fetchPages());
+store.dispatch(fetchPosts());
 
 // Google sheet data from NLM cataloguing
-store.dispatch(getGS())
+store.dispatch(getGS());
 
 // check connection and get ES data
-if(checkConnection()) {
-    store.dispatch(fetchData())
-    store.dispatch(fetchAuthors())
-    store.dispatch(fetchTopics())
+if (checkConnection()) {
+  store.dispatch(fetchData());
+  store.dispatch(fetchAuthors());
+  store.dispatch(fetchTopics());
 } else {
-    console.log("Can't make connection to ES...")
+  console.log("Can't make connection to ES...");
 }
-
 
 const App = () => {
-    document.body.classList.add('landing')
-    return (
-        <div className="container">
-            <React.StrictMode>
-                <Provider store={store}>
-                    <Router> {/* history={history}> */}
-                        <Route component={RouteSwitch} />
-                    </Router>
-                </Provider>
-            </React.StrictMode>
-        </div>
-    )
-}
+  document.body.classList.add("landing");
+  return (
+    <div className="container">
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            {" "}
+            {/* history={history}> */}
+            <Route component={RouteSwitch} />
+          </Router>
+        </Provider>
+      </React.StrictMode>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,94 +1,116 @@
-import { SET_LANG, SET_PAGE, SET_COLLAPSE, SET_BROWSE, SET_COLLECTION } from './types'
+import {
+  SET_LANG,
+  SET_PAGE,
+  SET_COLLAPSE,
+  SET_BROWSE,
+  SET_COLLECTION,
+  REQUEST_COLLECTIONS
+  //RECEIVE_COLLECTIONS,
+  //ERROR_COLLECTIONS
+} from "./types";
 
-export * from './searchActions'
-export * from './googleSheetsActions'
-export * from './WPPagesActions'
-export * from './WPPostsActions'
-export * from './utilityActions'
-export * from './ontology'
+import expressURL from "../apis/express";
 
-export const buildYear = '2018'
-export const defaultLanguage = 'en'
+export * from "./searchActions";
+export * from "./googleSheetsActions";
+export * from "./WPPagesActions";
+export * from "./WPPostsActions";
+export * from "./utilityActions";
+export * from "./ontology";
+
+export const buildYear = "2018";
+export const defaultLanguage = "en";
 export const languages = {
   en: "English",
   mn: "Mongolian"
-}
+};
 
-export const pages = [ 'home', 'archives' ]
+export const pages = ["home", "archives"];
 
-
-export const browseOptions = ['Title', 'Author', 'Subject']
+export const browseOptions = ["Title", "Author", "Subject"];
 export const browseOptionsObj = [
-  {browse: 'Title', stateType: 'esWorks'},
-  {browse: 'Author', stateType: 'esAuthors'},
-  {browse: 'Subject', stateType: 'esSubjects'}
-]
+  { browse: "Title", stateType: "esWorks" },
+  { browse: "Author", stateType: "esAuthors" },
+  { browse: "Subject", stateType: "esSubjects" }
+];
 
 export const bdrGender = {
   "bdr:GenderMale": "Male",
   "bdr:GenderFemale": "Female"
-}
+};
 
 export const bdrObjectType = {
   "bdr:ObjectTypeBlockprint": "Blockprint"
-}
+};
 
-export const bdrPurlURL = `http://purl.bdrc.io/resource/`
+export const bdrPurlURL = `http://purl.bdrc.io/resource/`;
 
 export const collections = [
-  {key: '1', name: 'Collection 1'},
-  {key: '2', name: 'Collection 2'}
-]
+  { key: "1", name: "Collection 1" },
+  { key: "2", name: "Collection 2" }
+];
 
 export const searchParams = {
-  initialIndex: 'v1_bdrc_work',
-  initialIndexPrefix: 'v1_bdrc_',
-  type: '_doc',
+  initialIndex: "v1_bdrc_work",
+  initialIndexPrefix: "v1_bdrc_",
+  type: "_doc",
   size: 10
-}
+};
 
-export const v1Indices = ['v1_*']
-export const v2Indices = ['v2_*']
+export const v1Indices = ["v1_*"];
+export const v2Indices = ["v2_*"];
 
 const IIIFImageAPI = {
-  region: 'full',
-  size: 'max', 
-  rotation: '0',
-  qualityAndFormat: 'default.jpg'
-}
+  region: "full",
+  size: "max",
+  rotation: "0",
+  qualityAndFormat: "default.jpg"
+};
 
-export const IIIFsuffix = 
-  `/${IIIFImageAPI.region}/${IIIFImageAPI.size}/${IIIFImageAPI.rotation}/${IIIFImageAPI.qualityAndFormat}`
+export const IIIFsuffix = `/${IIIFImageAPI.region}/${IIIFImageAPI.size}/${IIIFImageAPI.rotation}/${IIIFImageAPI.qualityAndFormat}`;
+
+export const fetchCollections = () => async dispatch => {
+  let endpoint = "/collections";
+  dispatch({ type: REQUEST_COLLECTIONS });
+  try {
+    const response = await expressURL.get(endpoint);
+    console.log("collections response", response);
+    //dispatch({ type: RECEIVE_COLLECTIONS, payload: response });
+  } catch (error) {
+    console.log(error);
+    //dispatch({ type: ERROR_COLLECTIONS, payload: error });
+  }
+};
 
 export const setLanguage = language => ({
   type: SET_LANG,
   language
-})
+});
 
 export const setLanguage2 = (language, i18n) => {
-  i18n.changeLanguage(language)
+  i18n.changeLanguage(language);
   return {
     type: SET_LANG,
     language
-  }
-}
+  };
+};
 
 export const setPage = page => ({
-    type: SET_PAGE,
-    page
-})
+  type: SET_PAGE,
+  page
+});
 
 export const setCollapse = collapse => ({
   type: SET_COLLAPSE,
   collapse
-})
+});
 
 export const setCollection = collection => ({
   type: SET_COLLECTION,
   collection
-})
+});
 
 export const setBrowse = browse => ({
   type: SET_BROWSE,
   browse
-})
+});

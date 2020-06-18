ac10b5f35beb8f7e0035e768acf3011360b4745b
joelcrawford <joelrootcrawford@gmail.com>
2020-06-18 16:22:43 -0700

    rebuild after adding to gitignore

---
+++ build/precache-manifest.fcaddf8f0be3925ee399057d5ab6f02c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c513354a73ce1ac33e5b4093b7581eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27decbe144544f03f1dd",
+    "url": "/static/css/2.d5f077cb.chunk.css"
+  },
+  {
+    "revision": "798d3ae128c85c164150",
+    "url": "/static/css/main.2ccfcdd7.chunk.css"
+  },
+  {
+    "revision": "27decbe144544f03f1dd",
+    "url": "/static/js/2.148e4e85.chunk.js"
+  },
+  {
+    "revision": "1689b70987aa8253fc4ea4e83bffb503",
+    "url": "/static/js/2.148e4e85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "798d3ae128c85c164150",
+    "url": "/static/js/main.4d642ca1.chunk.js"
+  },
+  {
+    "revision": "c81a15108d66d72ee482",
+    "url": "/static/js/runtime-main.dc721f8f.js"
+  },
+  {
+    "revision": "67ac4b0f05e70db2662fe1400136a717",
+    "url": "/static/media/UB-woodblock-closeup.67ac4b0f.jpg"
+  },
+  {
+    "revision": "35da14155da5117ed984d40b646bb5ef",
+    "url": "/static/media/logo_all.35da1415.png"
+  },
+  {
+    "revision": "c44b13b061229fbc840239c15dbdeb58",
+    "url": "/static/media/nlm_logo_full_transparent.c44b13b0.png"
+  },
+  {
+    "revision": "f3d5d89ff8bc450139680fdc319e8af3",
+    "url": "/static/media/nlm_logo_transparent.f3d5d89f.png"
+  }
+]);
